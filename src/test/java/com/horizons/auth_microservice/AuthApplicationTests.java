a98372c2a1abd43bd4f32c242de7666618533fd2
Rachid Abadli <rabadli@localhost.localdomain>
2020-08-25 00:41:46 -0700

    Initial commit

---
+++ src/test/java/com/horizons/auth_microservice/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horizons.auth_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
